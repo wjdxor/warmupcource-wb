3b04b6ae03763eeee0bd612b7d43ad26c4f8b70f
cyh <cyh@mydata.re.kr>
2023-05-09 14:12:53 +0900

    feat.패키지추가

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
 import styles from '@/styles/Home.module.css'
+import { RecoilRoot } from 'recoil'
 
 export default function Home() {
   return (
     <>
+    <RecoilRoot>
         <div className={styles.description}>
           <p>
             Let's Warm up!!&nbsp;
             <code className={styles.code}>Next.JS</code>
           </p>
           <div>
-            <a
+            {/* <a
               href="http://mydata.re.kr"
-            >
-              By MyDATA
+            > */}
+            <a href="/user/login">
+              {/* By MyDATA */}
+              Login
             </a>
           </div>
         </div>
+    <RecoilRoot/>
     </>
   )
 }
