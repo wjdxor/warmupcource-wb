6ff66e5ced34c85ad645f878304a655c8c565d5c
JT <jtlim@mydata.re.kr>
2023-05-09 16:42:22 +0900

    feat. recoil state 설정

---
+++ recoil/auth.js
@@ -0,0 +1,21 @@
+import { atom } from 'recoil';
+import {recoilPersist} from "recoil-persist";
+
+const {persistAtom} = recoilPersist();
+export const accessTokenState = atom({
+    key: 'accessToken',
+    default: '',
+    effects_UNSTABLE: [persistAtom]
+})
+
+export const refreshTokenState = atom({
+    key: 'refreshToken',
+    default: '',
+    effects_UNSTABLE: [persistAtom]
+})
+
+export const isLoggedIn = atom({
+    key: 'isLoggedIn',
+    default: false,
+    effects_UNSTABLE: [persistAtom]
+})
