8c311c94b2867e9c53950e5838cd15d692fcf07c
JT <jtlim@mydata.re.kr>
2023-05-09 16:42:07 +0900

    feat. recoil 설치 및 RecoilRoot 설정

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import '@/styles/globals.css'
 
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
 import Layout from '@/components/layout/layout'
+import {RecoilRoot} from "recoil";
 
-export default function App({ Component, pageProps }) {
-  const queryClient = new QueryClient();
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={true} />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </QueryClientProvider>
-  )
+export default function App({Component, pageProps}) {
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={true}/>
+            <RecoilRoot>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </RecoilRoot>
+        </QueryClientProvider>
+    )
 }
